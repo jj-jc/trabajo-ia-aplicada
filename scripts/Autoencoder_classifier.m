416a75f3a81e97b49abe3f17bf408cd41bb94c5c
Juanjojc <juanjojuradocaminod.v@gmail.com>
2021-06-20 19:41:24 +0200

    Some changes in autoencoder and fisher

--- scripts/Autoencoder_classifier.m
+++ scripts/Autoencoder_classifier.m
@@ -66,60 +66,60 @@
 % end
 
 %% Classification with autoencoder and softmax (95.3%)
-% % %% Load the data
-% load ../data/Trainnumbers.mat
-% Indexes = randperm(10000);
-% Training_Set.image = Trainnumbers.image(:,Indexes(1:8000));
-% Training_Set.label = Trainnumbers.label(1,Indexes(1:8000));
-% Testing_Set.image = Trainnumbers.image(:,Indexes(8001:end));
-% Testing_Set.label = Trainnumbers.label(:,Indexes(8001:end));
-% 
-% [image_n,ps0] = mapstd(Training_Set.image);
-% [test_n,ps1] = mapstd(Testing_Set.image);
-% %first autoencoder
-% rng('default')
-% hiddenSize1 = 100;
-% autoenc1 = trainAutoencoder(image_n,hiddenSize1, ...
-%     'MaxEpochs',400, ...
-%     'L2WeightRegularization',0.004, ...
-%     'SparsityRegularization',4, ...
-%     'SparsityProportion',0.15, ...
-%     'ScaleData', false);
-% view(autoenc1)
-% figure()
-% plotWeights(autoenc1);
-% feat1 = encode(autoenc1,image_n);
-% %Second autoencoder
-% hiddenSize2 = 50;
-% autoenc2 = trainAutoencoder(feat1,hiddenSize2, ...
-%     'MaxEpochs',100, ...
-%     'L2WeightRegularization',0.002, ...
-%     'SparsityRegularization',4, ...
-%     'SparsityProportion',0.1, ...
-%     'ScaleData', false);
-% view(autoenc2)
-% feat2 = encode(autoenc2,feat1);
-% 
-% %Training softmax layer
-% number_classes=10;
-% dim_training = size(Training_Set.label,2);
-% training_labels = zeros(number_classes, dim_training);
-% for i=1:dim_training
-%     training_labels(Training_Set.label(i)+1,i) = 1;
-% end
-% 
-% number_classes=10;
-% dim_test = size(Testing_Set.label,2);
-% test_labels = zeros(number_classes, dim_test);
-% for i=1:dim_test
-%     test_labels(Testing_Set.label(i)+1,i) = 1;
-% end
-% softnet = trainSoftmaxLayer(feat2, training_labels,'MaxEpochs',400);
-% stackednet = stack(autoenc1,autoenc2,softnet);
-% view(stackednet)
-% y = stackednet(test_n);
-% plotconfusion(test_labels,y);
-% % backpropagation
-% stackednet = train(stackednet, image_n, training_labels);
-% y = stackednet(test_n);
-% plotconfusion(test_labels,y);
+% %% Load the data
+load ../data/Trainnumbers.mat
+Indexes = randperm(10000);
+Training_Set.image = Trainnumbers.image(:,Indexes(1:8000));
+Training_Set.label = Trainnumbers.label(1,Indexes(1:8000));
+Testing_Set.image = Trainnumbers.image(:,Indexes(8001:end));
+Testing_Set.label = Trainnumbers.label(:,Indexes(8001:end));
+
+[image_n,ps0] = mapstd(Training_Set.image);
+[test_n,ps1] = mapstd(Testing_Set.image);
+%first autoencoder
+rng('default')
+hiddenSize1 = 100;
+autoenc1 = trainAutoencoder(image_n,hiddenSize1, ...
+    'MaxEpochs',400, ...
+    'L2WeightRegularization',0.004, ...
+    'SparsityRegularization',4, ...
+    'SparsityProportion',0.15, ...
+    'ScaleData', false);
+view(autoenc1)
+figure()
+plotWeights(autoenc1);
+feat1 = encode(autoenc1,image_n);
+%Second autoencoder
+hiddenSize2 = 50;
+autoenc2 = trainAutoencoder(feat1,hiddenSize2, ...
+    'MaxEpochs',100, ...
+    'L2WeightRegularization',0.002, ...
+    'SparsityRegularization',4, ...
+    'SparsityProportion',0.1, ...
+    'ScaleData', false);
+view(autoenc2)
+feat2 = encode(autoenc2,feat1);
+
+%Training softmax layer
+number_classes=10;
+dim_training = size(Training_Set.label,2);
+training_labels = zeros(number_classes, dim_training);
+for i=1:dim_training
+    training_labels(Training_Set.label(i)+1,i) = 1;
+end
+
+number_classes=10;
+dim_test = size(Testing_Set.label,2);
+test_labels = zeros(number_classes, dim_test);
+for i=1:dim_test
+    test_labels(Testing_Set.label(i)+1,i) = 1;
+end
+softnet = trainSoftmaxLayer(feat2, training_labels,'MaxEpochs',400);
+stackednet = stack(autoenc1,autoenc2,softnet);
+view(stackednet)
+y = stackednet(test_n);
+plotconfusion(test_labels,y);
+% backpropagation
+stackednet = train(stackednet, image_n, training_labels);
+y = stackednet(test_n);
+plotconfusion(test_labels,y);
--- scripts/Fischer.m
+++ scripts/Fischer.m
@@ -1,5 +1,6 @@
 %% Linear Fishcer Disciminant for reducing the dimensionality
 % Load the data
+rng('default')%this restat the seed for the random values
 load ../data/Trainnumbers.mat
 Indexes = randperm(10000);
 Training_Set.image = Trainnumbers.image(:,Indexes(1:8000));
@@ -8,9 +9,7 @@ Testing_Set.image = Trainnumbers.image(:,Indexes(8001:end));
 Testing_Set.label = Trainnumbers.label(:,Indexes(8001:end));
 % Normalization
 [image_n,ps0] = mapstd(Training_Set.image);
-image_n = Training_Set.image;
-means = sum(image_n')';
-
+means = mean(image_n')';
 % Divide the images in their corresponding class
 class_0 = image_n(:,(Training_Set.label==0));
 class_1 = image_n(:,(Training_Set.label==1));
@@ -31,17 +30,84 @@ classes_NUM = [size(class_0,2), size(class_1,2), size(class_2,2), ...
 
 SWn = 0;
 SBn = 0;
+% W = 0;
+% B = 0;
 for i=1:10
     %Sin
     Sin{i} = cov(classes{i}(:,:)',1)*classes_NUM(i);
     SWn = SWn + Sin{i};
-    mean_class = mean(classes{i}(:,:)')';
-    SBn = SBn + classes_NUM(i)*(mean_class - means)*(mean_class - means)';
+    mci = mean(classes{i}(:,:)')';
+    SBn = SBn + classes_NUM(i)*(mci - means)*(mci - means)';
+%     xi=classes{i}(:,:)-repmat(mci,[1 classes_NUM(i)]); %Xi-MeanXi
+%     W=W+xi*xi';                         %CALCULATE W
+%     B=B+classes_NUM(i)*(mci-means)'*(mci-means); %CALCULATE B
 end
-SWn = inv(SWn'*SWn)*SWn';
-Wn=inv(SWn)*(SBn);
+% %W = inv(W)*B;
+% 
+%clean of the SWn. There are many features that do not have relation with
+%the other features.
+% SWn_clear = zeros(784,784);
+% for i=1:784
+%     for j=1:784
+%         if SWn(i,j) == 0
+%             SWn(i,j) = 0.01;
+%         end
+%     end
+% end 
+% W = inv(SWn)*SBn;
+SWn_clear = SWn;
+SBn_clear = SBn;
+clean=0;
+for i=1:784
+    if sum(SWn(:,i))==0
+            SWn_clear(:,i-clean) = [];   
+            SWn_clear(i-clean,:) = []; 
+            SBn_clear(:,i-clean) = [];   
+            SBn_clear(i-clean,:) = []; 
+            clean = clean + 1;
+    end
+end
+W = inv(SWn_clear)*SBn_clear;
+[W,Diagn]=eig(W);
+%It is necessary to choose 10 -1 classes.
+selection = sum(Diagn);
+B = sort(selection,'descend');
+B = B(1:10);
+%It is necessary to choose 10 -1 classes.
+selection = sum(Diagn);
+D2 = sort(selection,'descend');
+D2 = D2(1:2);
+
+
+% SWn = SWn_clear;
+% clean=784;
+% insert=1;
+% for i=1:784
+%     if sum(SWn(i,:))==0
+%             SWn_clear(insert, :) = SWn(i,:);
+%             SWn_clear(clean, :) = [];
+%             insert = insert + 1;
+%             clean = clean - 1;
+%     end
+% end
+% SWn = SWn_clear;
+% S1 = cov(classes{1}(:,:)',1)*classes_NUM(1);
+% SWn = inv(SWn'*SWn)*SWn';
+% W = inv(W'*W)*W';
+% Wn=inv(SWn)*(SBn);
 % [image_n,ps0] = mapstd(Training_Set.image);
 % [test_n,ps1] = mapstd(Testing_Set.image);
+% 
+% S0n = cov(class_0',1)*classes_NUM(1);
+% S1n = cov(class_1',1)*classes_NUM(2);
+% S2n = cov(class_2',1)*classes_NUM(3);
+% S3n = cov(class_3',1)*classes_NUM(4);
+% S4n = cov(class_4',1)*classes_NUM(5);
+% S5n = cov(class_5',1)*classes_NUM(6);
+% S6n = cov(class_6',1)*classes_NUM(7);
+% S7n = cov(class_7',1)*classes_NUM(8);
+% S8n = cov(class_8',1)*classes_NUM(9);
+% S9n = cov(class_9',1)*classes_NUM(10);
+% SWn = S0n +S1n + S2n + S3n +S4n + S5n +S6n +S7n + S8n +S9n;
 
 %% Feature separability
-
